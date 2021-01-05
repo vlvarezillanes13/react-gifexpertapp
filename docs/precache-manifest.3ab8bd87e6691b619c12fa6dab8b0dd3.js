247a14543623b84c2ba601616ead231cec2d8ae5
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2021-01-05 15:11:40 -0300

    Docs agregado

---
+++ docs/precache-manifest.3ab8bd87e6691b619c12fa6dab8b0dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ef307867a8847cfe90ee7dec6afab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a56f3765e36ff413b93",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a56f3765e36ff413b93",
+    "url": "/static/js/main.ef86eb69.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
